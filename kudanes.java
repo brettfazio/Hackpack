943fbd6bcee8f2143daca1c2e36067ca59c62a25
Brett <brett.fazio@hotmail.com>
2016-11-07 12:10:52 -0500

    Add files via upload

---
+++ kudanes.java
@@ -0,0 +1,38 @@
+// Brett Fazio, Kudane's Algoritm, Max range in 1D[]
+
+public class kudanes {
+
+	public static void main(String[] args) {
+
+		int[] arr = {100, 100, 100, -100, -1000};
+
+		int running = 0;
+		int max = Integer.MIN_VALUE;
+		int l = 0, r = 0;
+		int ml = 0, mr = 0;
+		for (int i= 0; i < arr.length; i++) {
+			running += arr[i];
+			r++;
+
+
+
+			if (running > max) {
+				ml = l;
+				mr = r;
+				max = running;
+			}else {
+			}
+
+
+			if (running < 0) {
+				running = 0;
+				l = i+1;
+			}
+		}
+		ml++; // for zero basing
+		mr++;
+		System.out.println(max + " " + ml + " " + mr);
+		// ML = Inclusive, MR = Exclusive
+	}
+
+}
