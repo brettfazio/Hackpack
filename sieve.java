e44a1c9bf23bd7b926eed2b0346fad00e2d9534b
Brett <brett.fazio@hotmail.com>
2016-11-08 12:21:41 -0500

    Add files via upload

---
+++ sieve.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class sieve {
+	public static void main(String[] args) {
+		int MAX = 10_000_000;
+		boolean[] isPrime = new boolean[MAX];
+		
+		Arrays.fill(isPrime, true);
+		isPrime[0] = false;
+		isPrime[1] = false;		
+		
+		// We stop at the square, and don't redivide for composites.
+		for (int i=2; i<Math.sqrt(MAX)+1; i++)
+			if (isPrime[i])
+				for (int j=2*i; j<MAX; j+=i)
+					isPrime[j] = false;	
+		
+		System.out.println(isPrime[16]);
+	}
+}
