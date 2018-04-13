06dd4d051fbd0e09ef48b8794f2c1b80cdf75a4c
Brett <brett.fazio@hotmail.com>
2018-04-13 11:09:49 -0400

    Update and rename anagramcounting.java to AnagramCounting.java

--- AnagramCounting.java
+++ AnagramCounting.java
@@ -2,7 +2,7 @@ import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class anagramcounting {
+public class AnagramCounting {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
