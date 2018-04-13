83175d994f871f0710b85e4e8391e3ee7d8b2de5
Brett <brett.fazio@hotmail.com>
2018-04-13 11:12:04 -0400

    Update and rename towersOfHanoi.java to TowersOfHanoi.java

--- TowersOfHanoi.java
+++ TowersOfHanoi.java
@@ -1,7 +1,7 @@
 // Ahmad Barhamje & Brett Fazio, Towers of Hanoi
 
 
-public class towersOfHanoi {
+public class TowersOfHanoi {
 	public static void main(String[] args) {
 		//takes 2^n - 1 moves to solve a tower of hanoi with n disks.
 		solve(3, 'A', 'B', 'C');
