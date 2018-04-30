ac549cdfe0d48a5a51abdba432ebbc6c8d24c7da
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2018-04-29 22:10:52 -0400

    Update Sieve.java

--- Sieve.java
+++ Sieve.java
@@ -2,18 +2,21 @@ import java.util.*;
 
 public class Sieve {
 	public static void main(String[] args) {
-		int MAX = 10_000_000;
-		boolean[] isPrime = new boolean[MAX];
+		int max = 10_000_001;
+		boolean[] isPrime = new boolean[max];
 		
 		Arrays.fill(isPrime, true);
 		isPrime[0] = false;
-		isPrime[1] = false;		
+		isPrime[1] = false;
 		
 		// We stop at the square, and don't redivide for composites.
-		for (int i=2; i<Math.sqrt(MAX)+1; i++)
-			if (isPrime[i])
-				for (int j=2*i; j<MAX; j+=i)
-					isPrime[j] = false;	
+		for(int i = 2; i < Math.sqrt(max) + 1; i++){
+			if(isPrime[i]){
+				for(int j = i * 2; j < max; j += i){
+					isPrime[j] = false;
+				}
+			}
+		}
 		
 		System.out.println(isPrime[16]);
 	}
