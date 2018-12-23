dd07e7b390b9ab3790c1e50d83900b6f99e49948
Brett <brett.fazio@hotmail.com>
2018-12-22 20:31:11 -0500

    Update AreaOfPolygon.java

--- AreaOfPolygon.java
+++ AreaOfPolygon.java
@@ -2,24 +2,22 @@ public class AreaOfPolygon {
 
 	public static void main(String[] args) {
 		
-		int NUM = 5;
+		int n = 5;
 		
-		double[] x = new double[NUM];
-		double[] y = new double[NUM];
+		double[] x = new double[n];
+		double[] y = new double[n];
 		
 		double adder = 0;
 		
-		for (int i = 0; i < NUM; i++) {
-			if (i == NUM-1) {
-				adder += x[i] * y[0];
-				adder += y[i] * x[0];
-			}else {
-				adder += x[i] * y[i+1];
-				adder += y[i] * x[i+1];
-			}
-		}
-		
-		adder /= 2.0;
+        	// Calculate value of shoelace formula 
+	        int j = n - 1; 
+        	for (int i = 0; i < n; i++) 
+        	{ 
+			adder += (X[j] + X[i]) * (Y[j] - Y[i]); 
+			j = i;  
+        	} 
+      
+		adder = Math.abs(adder/2.0);
 		
 		double res = adder;
 		
