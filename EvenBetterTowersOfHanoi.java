2fdbd729a3459c176019a1591bcab5dffb26b8be
Xylenox <37784637+Xylenox@users.noreply.github.com>
2019-02-26 14:32:47 -0500

    Create EvenBetterTowersOfHanoi.java

---
+++ EvenBetterTowersOfHanoi.java
@@ -0,0 +1,25 @@
+/*
+Don't use the other bad TowersOfHanois.
+They're bad.
+*/
+import java.util.Scanner;
+public class BetterTowersOfHanoi {
+	static String[][][] dp = new String[1_000_000]['Z'+1]['Z'+1]['Z'+1];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.print(solve(n, 'A', 'B', 'C'));
+	}
+	static String solve(int n, char start, char medium, char end){
+		if(dp[n][start][medium][end] != null) return dp[n][start][medium][end];
+		if(n == 1){
+			return dp[n][start][medium][end] = start + "->" + end + "\n";
+		}
+		else{
+			dp[n][start][medium][end] = solve(n - 1, start, end, medium);
+			dp[n][start][medium][end] += start + "->" + end + "\n";
+			dp[n][start][medium][end] += solve(n - 1, medium, start, end);
+			return dp[n][start][medium][end];
+		}
+	}
+}
