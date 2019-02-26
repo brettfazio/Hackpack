c0429d64c8844ebe3aa7ea375fbfc2ea662dc217
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2019-02-26 09:18:57 -0500

    Update Floyd.java

--- Floyd.java
+++ Floyd.java
@@ -2,25 +2,23 @@ class Floyd {
     static int n, m;
     static long inf = Long.MAX_VALUE / 2;
     static long[][] d;
-	public static void main (String[] args){
-	    int n, m;
-	    d = new long[n][n];
-	    for(int i = 0; i < n; ++i) Arrays.fill(d[i], inf);
-	    for(int i = 0; i < n; ++i) d[i][i] = 0;
-	    
-	    //edges of graph
-	    for(int i = 0; i < m; ++i){
-	        int a, b, w;
-		//if bidirectional:
-	        if(w < d[a][b]) d[a][b] = d[b][a] = w;
-	    }
-	    
-	    for(int k = 0; k < n; ++k){
-	        for(int i = 0; i < n; ++i){
-	            for(int j = 0; j < n; ++j){
-	                d[i][j] = Math.min(d[i][j], d[i][k] + d[k][j]);
-	            }
-	        }
-	    }
-	}
+    public static void main (String[] args){
+        d = new long[n][n];
+        for(int i = 0; i < n; ++i) Arrays.fill(d[i], inf);
+        for(int i = 0; i < n; ++i) d[i][i] = 0;
+        
+        //edges of graph
+        for(int i = 0; i < m; ++i){
+            int a, b, w;
+            if(w < d[a][b]) d[a][b] = d[b][a] = w;
+        }
+        
+        for(int k = 0; k < n; ++k){
+            for(int i = 0; i < n; ++i){
+                for(int j = 0; j < n; ++j){
+                    d[i][j] = Math.min(d[i][j], d[i][k] + d[k][j]);
+                }
+            }
+        }
+    }
 }
