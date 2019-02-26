c691e91bedb0a23110e7dccaf89407722971a23d
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2019-02-26 09:17:12 -0500

    Update Floyd.java

--- Floyd.java
+++ Floyd.java
@@ -1,34 +1,26 @@
-// Brett Fazio, floyd warshall
-
-import java.util.*;
-
-public class Floyd {
-   	public static void main(String[] args) {
-   		// By default the min value is any number higher than the max value of the input
-   		Scanner sc = new Scanner(System.in);
-   		int nodes = sc.nextInt(), edges = sc.nextInt();
-   		// depending on bidirectionality dp[i][j] may or may not equal dp[j][i]
-   		// the code below is bidirectional
-
-   		int[][] dp = new int[nodes][nodes];
-   		
-   		for (int i = 0; i < dp.length; i++) {
-   			Arrays.fill(dp[i], 10000000);
-   		}
-   		for (int i = 0; i < dp.length; i++)
-   			for (int j = 0; j < dp.length; j++)
-   				dp[i][j] = i == j ? 0 : dp[i][j];
-   		
-   		for (int i = 0; i < edges; i++) {
-   			int a = sc.nextInt(), b = sc.nextInt(), weight = sc.nextInt();
-   			dp[a][b] = dp[b][a] = weight;
-   		}
-   		for (int k = 0; k < nodes; k++) {
-			for (int i = 0; i < nodes; i++) { // i
-				for (int j = 0; j < nodes; j++) {
-					dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
-				}
-			}
-		}
-  	}
+class Floyd {
+    static int n, m;
+    static long inf = Long.MAX_VALUE / 2;
+    static long[][] d;
+	public static void main (String[] args){
+	    int n, m;
+	    d = new long[n][n];
+	    for(int i = 0; i < n; ++i) Arrays.fill(d[i], inf);
+	    for(int i = 0; i < n; ++i) d[i][i] = 0;
+	    
+	    //edges of graph
+	    for(int i = 0; i < m; ++i){
+	        int a, b, w;
+		//if bidirectional:
+	        if(w < d[a][b]) d[a][b] = d[b][a] = w;
+	    }
+	    
+	    for(int k = 0; k < n; ++k){
+	        for(int i = 0; i < n; ++i){
+	            for(int j = 0; j < n; ++j){
+	                d[i][j] = Math.min(d[i][j], d[i][k] + d[k][j]);
+	            }
+	        }
+	    }
+	}
 }
